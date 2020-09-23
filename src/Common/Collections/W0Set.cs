15a96072fc942359bf404fcc533df2c7fd4beb9e
Sawyer Watts <sawyer.watts@gmail.com>
2020-09-23 11:51:08 -0700

    Create W0Set, a child of HashSet with .Equals overridden.

---
+++ src/Common/Collections/W0Set.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+
+namespace WorldZero.Common.Collections
+{
+    /// <summary>
+    /// W0Set<T> is a subclass of HashSet<T>. This class makes the addition of
+    /// overriding `Equals(object)` and `GetHashCode`.
+    /// </summary>
+    /// <remarks>
+    /// The time complexity of both of these operations is O(n).
+    /// </remarks>
+    public class W0Set<T> : HashSet<T>
+    {
+        public override int GetHashCode()
+        {
+            int r = 1;
+            foreach (T t in this)
+                r *= t.GetHashCode();
+            return r;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || (obj.GetType() != this.GetType()))
+                return false;
+
+            var other = (W0Set<T>) obj;
+            if (this.Count != other.Count)
+                return false;
+
+            var thisValues = this.GetEnumerator();
+            var otherValues = other.GetEnumerator();
+
+            while (thisValues.MoveNext() && otherValues.MoveNext())
+            {
+                if ((ReferenceEquals(thisValues.Current, null))
+                    ^ (ReferenceEquals(otherValues.Current, null)))
+                {
+                    return false;
+                }
+
+                if ((thisValues.Current != null)
+                    && (!thisValues.Current.Equals(otherValues.Current)))
+                {
+                    return false;
+                }
+            }
+            return !thisValues.MoveNext() && !otherValues.MoveNext();
+        }
+    }
+}
---
+++ test/Unit/Common/Collections/TestW0Set.cs
@@ -0,0 +1,34 @@
+using WorldZero.Common.Collections;
+using NUnit.Framework;
+
+namespace WorldZero.Test.Unit.Common.Collections
+{
+    [TestFixture]
+    public class TestW0Set
+    {
+        [Test]
+        public void TestEquals()
+        {
+            var set0 = new W0Set<int>();
+            set0.Add(3);
+            set0.Add(7);
+            set0.Add(9);
+            set0.Add(9);
+            set0.Add(324);
+
+            var set1 = new W0Set<int>();
+            set1.Add(3);
+            set1.Add(3);
+            set1.Add(7);
+            set1.Add(9);
+            set1.Add(324);
+
+            Assert.AreEqual(set0, set1);
+            set1.Add(-932);
+            Assert.AreNotEqual(set0, set1);
+
+            Assert.IsFalse(set0.Equals(null));
+            Assert.IsFalse(set0.Equals("string"));
+        }
+    }
+}
