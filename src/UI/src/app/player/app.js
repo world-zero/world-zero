93d1c3b5d0f8ad080d133f6e1762b945518aefdc
aboueich <47013540+aboueich@users.noreply.github.com>
2020-10-11 18:57:58 -0700

    updates to allow JS scripts to run

---
+++ src/UI/src/app/player/app.js
@@ -0,0 +1,73 @@
+
+function addRow() {
+
+  var myName = document.getElementById("Player Name");
+  var points = document.getElementById("Points");
+  var level = document.getElementById("Level");
+  var group = document.getElementById("Affiliation");
+
+
+  var rowCount = table.rows.length;
+  var row = table.insertRow(rowCount);
+
+  row.insertCell(0).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
+  row.insertCell(1).innerHTML= myName.value;
+  row.insertCell(2).innerHTML= points.value;
+  row.insertCell(3).innerHTML= level.value;
+  row.insertCell(4).innerHTML= group.value;
+
+
+
+}
+
+function deleteRow(obj) {
+
+  var index = obj.parentNode.parentNode.rowIndex;
+  var table = document.getElementById("myTableData");
+  table.deleteRow(index);
+
+}
+
+function addTable() {
+
+  var myTableDiv = document.getElementById("myDynamicTable");
+
+  var table = document.createElement('TABLE');
+  table.border='1';
+
+  var tableBody = document.createElement('TBODY');
+  table.appendChild(tableBody);
+
+  for (var i=0; i<3; i++){
+    var tr = document.createElement('TR');
+    tableBody.appendChild(tr);
+
+    for (var j=0; j<4; j++){
+      var td = document.createElement('TD');
+      td.width='75';
+      td.appendChild(document.createTextNode("Cell " + i + "," + j));
+      tr.appendChild(td);
+    }
+  }
+  myTableDiv.appendChild(table);
+
+}
+
+function load() {
+
+  console.log("Page load finished");
+
+}
+
+function toggleMenu() {
+  var menu = document.getElementById("taskForm");
+  var buttonText = document.getElementById("addTask")
+  if(menu.style.display === "none"){
+    menu.style.display = "block";
+    buttonText.innerHTML = "Nevermind!"
+  } else {
+    menu.style.display = "none";
+    buttonText.innerHTML = "Add a task!"
+  }
+
+}
