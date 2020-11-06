4250fedd3cb638e44e2a060d5bf65753849956e7
Sawyer Watts <sawyer.watts@gmail.com>
2020-11-05 19:32:58 -0800

    Create IPraxisRepo.GetPraxisCount(Id, ISet<Name>)
    This is done to allow for the ability to check how many active / in progress
    praxises a character has.

--- src/Data/Interface/Repository/Entity/IPraxisRepo.cs
+++ src/Data/Interface/Repository/Entity/IPraxisRepo.cs
@@ -1,9 +1,24 @@
 using WorldZero.Common.Entity;
+using WorldZero.Common.ValueObject.General;
+using System.Collections.Generic;
 
 namespace WorldZero.Data.Interface.Repository.Entity
 {
     /// <inheritdoc cref="IIdEntityRepo"/>
     public interface IPraxisRepo
         : IIdEntityRepo<Praxis>
-    { }
+    {
+        /// <summary>
+        /// This will return the number of praxises a character has with
+        /// statuses that exist in `statuses`.
+        /// </summary>
+        /// <returns>
+        /// This will return 0 if there are no statuses in the set.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// This is thrown if the related table for `PraxisParticipant`s cannot
+        /// be found.
+        /// </exception>
+        int GetPraxisCount(Id characterId, ISet<Name> statuses);
+    }
 }
