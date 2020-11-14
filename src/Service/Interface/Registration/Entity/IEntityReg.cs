e983808e882a9b018bd097cc7779b20d1888af4c
Sawyer Watts <sawyer.watts@gmail.com>
2020-11-14 13:03:04 -0800

    Create IEntityService

--- src/Service/Interface/Registration/Entity/IEntityReg.cs
+++ src/Service/Interface/Registration/Entity/IEntityReg.cs
@@ -1,43 +1,19 @@
 using System;
 using System.Threading.Tasks;
 using WorldZero.Common.Interface;
-using WorldZero.Data.Interface.Repository.Entity;
 using WorldZero.Common.Interface.Entity;
+using WorldZero.Data.Interface.Repository.Entity;
 
 namespace WorldZero.Service.Interface.Registration.Entity
 {
-    /// <summary>
-    /// This is a generic interface for entity creation service classes.
-    /// </summary>
-    /// <typeparam name="TEntity">
-    /// The IEntity implementation that this class abstracts.
-    /// </typeparam>
-    /// <typeparam name="TId">
-    /// This is the `ISingleValueObject` implementation that `Entity` uses as
-    /// an ID.
-    /// </typeparam>
-    /// <typeparam name="TBuiltIn">
-    /// This is the built-in type behind `IdType`.
-    /// </typeparam>
-    /// <remarks>
-    /// For ease of use, it is recommended to have a property similar to the
-    /// below to easily and consistently cast up in children. This example is
-    /// taken from CreateEra.
-    /// <code>
-    /// protected IEraRepo _eraRepo { get { return (IEraRepo) this._repo; } }
-    /// </code>
-    /// </remarks>
     public abstract class IEntityReg<TEntity, TId, TBuiltIn>
+        : IEntityService<TEntity, TId, TBuiltIn>
         where TEntity : IEntity<TId, TBuiltIn>
         where TId : ISingleValueObject<TBuiltIn>
     {
-        protected readonly IEntityRepo<TEntity, TId, TBuiltIn> _repo;
-
         protected IEntityReg(IEntityRepo<TEntity, TId, TBuiltIn> repo)
-        {
-            this.AssertNotNull(repo, "repo");
-            this._repo = repo;
-        }
+            : base(repo)
+        { }
 
         /// <summary>
         /// This will store the supplied entity and save the repo.
@@ -61,22 +37,6 @@ namespace WorldZero.Service.Interface.Registration.Entity
         public virtual async Task<TEntity> RegisterAsync(TEntity e)
         {
             return await Task.Run(() => this.Register(e));
-        }
-
-        protected void AssertNotNull(object o, string name)
-        {
-            if (o == null)
-                throw new ArgumentNullException(name);
-        }
-
-        protected void EnsureExists(TEntity e)
-        {
-            try
-            {
-                this._repo.Insert(e);
-                this._repo.Save();
-            }
-            catch (ArgumentException) { }
         }
     }
 }
---
+++ test/Integration/Service/Interface/Registration/Entity/TestIEntityService.cs
@@ -0,0 +1,62 @@
+using System;
+using NUnit.Framework;
+using WorldZero.Common.Entity;
+using WorldZero.Common.ValueObject.General;
+using WorldZero.Data.Interface.Repository.Entity;
+using WorldZero.Data.Repository.RAM.Entity;
+using WorldZero.Service.Interface.Registration.Entity;
+
+namespace WorldZero.Test.Integration.Service.Interface.Registration.Entity
+{
+    [TestFixture]
+    public class TestIEntityService
+    {
+        private RAMFlagRepo _repo;
+        private TestEntityService _service;
+
+        [SetUp]
+        public void Setup()
+        {
+            this._repo = new RAMFlagRepo();
+            this._service = new TestEntityService(this._repo);
+        }
+
+        [Test]
+        public void TestConstructor()
+        {
+            Assert.Throws<ArgumentNullException>(()=>
+                new TestEntityService(null));
+
+            var repo = new RAMFlagRepo();
+            var service = new TestEntityService(repo);
+            Assert.IsNotNull(service.Repo);
+            Assert.AreEqual(repo, service.Repo);
+        }
+
+        [Test]
+        public void TestEnsureExists()
+        {
+            var f = new Flag(new Name("dne"));
+            Assert.Throws<ArgumentException>(()=>this._repo.GetById(f.Id));
+            this._service.EnsureExists(f);
+            Assert.IsNotNull(this._repo.GetById(f.Id));
+            this._service.EnsureExists(f);
+            Assert.IsNotNull(this._repo.GetById(f.Id));
+        }
+    }
+
+    public class TestEntityService
+        : IEntityService<Flag, Name, string>
+    {
+        public TestEntityService(IFlagRepo flagRepo)
+            : base(flagRepo)
+        { }
+
+        public IEntityRepo<Flag, Name, string> Repo { get { return this._repo; } }
+
+        public new void EnsureExists(Flag p)
+        {
+            base.EnsureExists(p);
+        }
+    }
+}
