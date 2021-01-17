e86f7ef7494eba0b8903df5e1b29e2ad239b11a2
Sawyer Watts <sawyer.watts@gmail.com>
2021-01-17 11:53:14 -0800

    Add design log and work log to IDTO.

--- src/Common/Interface/DTO/IDTO.cs
+++ src/Common/Interface/DTO/IDTO.cs
@@ -1,9 +1,30 @@
 using System;
 
+/* Goal
+    IDTO <- IEntityDTO <- IEntity <- IChar <- UnsafeChar
+                        <- ICharDTO <- CharDTO
+
+        where ICharDTO has immutable properties
+        where CharDTO has a dapper constructor (added to the DB repo implementation ticket)
+            repos should use entity DTOs instead of entities directly
+            reg service classes should take these whereas deletion/update should take entities
+                search services return proper entities
+                    what about if they got two versions of an entity and
+                            only updated one of them? or something like that
+        where IChar can have methods but not recommended
+        where UnsafeChar has an ICharDTO constructor and clones it into memory which the inherited properties get from
+            Be cautious of ICharDTO implementations that have a shallow Clone
+        where IEntiy.Clone() will return a clone *of the DTO* (return an IEntityDTO)
+            drop CloneAsEntity too
+*/
+
+// DONE: create unspecified DTOs
+// DONE: create the specified DTO IFs
 // DONE: create unspecified primary entity DTOs
 // DONE: create unspecified relation entity DTOs
 // DONE: create specified primary entity DTOs
 // TODO: create specified relation entity DTOs
+// TODO: add entity constructors w/ ones that take the corresponding DTO
 //
 // TODO: merge DTOs into entities
 //      this is going to be BIG
@@ -12,8 +33,24 @@ using System;
 //      have IEntity note that entities shouldn't override clone, so they'll just
 //          inherit a DTO clone from their concrete DTO parent
 //
-// TODO: migrate repos; this will cause Service to fail, but that's okay for now
-//      when mirgrated, remove the GetUniqRules stuffs from entities
+// TODO: migrate repos
+//      this will likely cause Service to fail, but that's okay for now
+//      when mirgrated, remove the GetUniqRules from entities
+//
+// TODO: migrage services to use DTOs where appropriate
+//      I{Enttiy}Reg should def use DTOs
+
+
+/* TODO: migrate entity implementations into their service IF
+    Create w0.test.tools which exposes these to the testing projects?
+
+    be sure to put the ABCs w/in IEntityService or whereever to make sure people arent casting
+        I may want to have that partial (?) class keyword where the class can be defined in several
+            locations so I can still keep them in independent files
+*/
+
+// TODO: make UMLs for entitise, DTOs, and possibly services
+
 
 namespace WorldZero.Common.Interface.DTO
 {
